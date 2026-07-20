71aa81efdd4bd7c41cf461faf9c4f318c37ff455
Rex Kenyon <112488983+xStat101x@users.noreply.github.com>
2026-07-20 15:24:08 -0400

    Add iOS Xcode project under /ios

---
+++ ios/LifeOS/LifeOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LifeOS
+//
+//  Created by Rex Kenyon on 7/20/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ios/LifeOS/LifeOS/LifeOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  LifeOSApp.swift
+//  LifeOS
+//
+//  Created by Rex Kenyon on 7/20/26.
+//
+
+import SwiftUI
+
+@main
+struct LifeOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
